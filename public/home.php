c9efe36dda2d5f78741b90356f3beeef72a8a2bc
jmoritdev <jmmt03@gmail.com>
2016-03-30 12:59:57 +0200

    did alot of stuff...
    implemented fontawesome, added new tables to database, added home.php,
    added logout .php.

---
+++ public/home.php
@@ -0,0 +1,17 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include "layout.php"; ?>
+    </head>
+    <body>
+        <div class="container">
+            <br><br><br>
+            <div>
+                <a class="btn btn-lg btn-info" href="mathassignment.php">Wiskunde <i class="fa fa-percent"></i></a>
+            </div>
+        </div>
+    </body>
+</html>
+
+
